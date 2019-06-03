b07500c8e3b019af8634722053368bd6b61adea2
garganshul108 <sci.agarg@gmail.com>
2019-06-03 12:43:00 +0530

    question added

---
+++ submatrix-sum-queries.cpp
@@ -0,0 +1,8 @@
+/**
+*
+Problem: 
+Sub-matrix Sum Queries
+Given a matrix of integers A of size N x M and multiple queries Q, for each query find and return the submatrix sum. Inputs to queries are top left (b, c) and bottom right (d, e) indexes of submatrix whose sum is to find out. Note: Rows are numbered from top to bottom and columns are numbered from left to right. Sum may be large so return the answer mod 10^9 + 7.
+
+
+*/
